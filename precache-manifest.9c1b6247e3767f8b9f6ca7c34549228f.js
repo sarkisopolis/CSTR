7eb003bef18e8210715007292ce1ff871eec54e8
sarkisopolis <59146679+sarkisopolis@users.noreply.github.com>
2020-04-23 14:50:25 -0400

    Add files via upload

---
+++ precache-manifest.9c1b6247e3767f8b9f6ca7c34549228f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1ec76a7d806a68f0f0a20b21f65129",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d68cfed68ef70fb520ee",
+    "url": "./static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d68cfed68ef70fb520ee",
+    "url": "./static/js/2.9b1c35d7.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "./static/js/2.9b1c35d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e6e24d0725b4d7a185",
+    "url": "./static/js/main.27436117.chunk.js"
+  },
+  {
+    "revision": "cff36fc719ac25ce05d1",
+    "url": "./static/js/runtime-main.f33ba7f4.js"
+  },
+  {
+    "revision": "0d0b345209913987e7e231d70d4ad09b",
+    "url": "./static/media/CSTR.0d0b3452.png"
+  }
+]);
